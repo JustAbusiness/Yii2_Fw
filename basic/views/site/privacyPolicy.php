491317bf9d2c06a3a19144349c0cc23f50460053
huysanti123456@gmail.com <102708893+justabusiness@users.noreply.github.com>
2024-06-30 17:36:09 +0700

    Initial commit

---
+++ basic/views/site/privacyPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'Privacy Policy';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-privacy-policy">
+    <h2><?= Html::encode($this->title) ?></h2>
+    <p>
+        This page is for privacy policy
+    </p>
+    <span>
+          This is the About page. You may modify the following file to customize its content:
+     </span>
+
+    <code><?= __FILE__ ?></code>
+</div>
