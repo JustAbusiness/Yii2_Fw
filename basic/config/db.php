00b74883fe5ab425154b5311162b05eef2a00fce
JustAbusinesse <huysanti123456@gmail.com>
2024-07-01 14:44:47 +0700

    create migratons and table in yii2

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=portfolio',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
@@ -10,5 +10,5 @@ return [
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    //'schemaCache' => 'cache
 ];
