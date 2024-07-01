63cb9067a33217565b0c078e0e5660f38367b41c
JustAbusinesse <huysanti123456@gmail.com>
2024-07-01 17:23:34 +0700

    create module

---
+++ basic/models/File.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "file".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $base_url
+ * @property string $mine_type
+ *
+ * @property Image[] $images
+ * @property Testimonial[] $testimonials
+ */
+class File extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'file';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'base_url', 'mine_type'], 'required'],
+            [['name', 'base_url', 'mine_type'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'base_url' => Yii::t('app', 'Base Url'),
+            'mine_type' => Yii::t('app', 'Mine Type'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Images]].
+     *
+     * @return \yii\db\ActiveQuery|ImageQuery
+     */
+    public function getImages()
+    {
+        return $this->hasMany(Image::class, ['file_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Testimonials]].
+     *
+     * @return \yii\db\ActiveQuery|TestimonialQuery
+     */
+    public function getTestimonials()
+    {
+        return $this->hasMany(Testimonial::class, ['customer_image_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return FileQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new FileQuery(get_called_class());
+    }
+}
--- basic/views/blog/index.php
+++ basic/views/blog/index.php
@@ -6,4 +6,4 @@
 <p>
     You may change the content of this page by modifying
     the file <code><?= __FILE__; ?></code>.
-</p>
+</p
